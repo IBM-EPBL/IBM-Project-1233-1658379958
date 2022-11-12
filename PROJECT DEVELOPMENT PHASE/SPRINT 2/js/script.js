97f1fcd231017906da62fbe7b6314a2a707ce2c4
mkharesh <87030202+mkharesh@users.noreply.github.com>
2022-11-12 22:18:54 +0530

    Add files via upload

---
+++ PROJECT DEVELOPMENT PHASE/SPRINT 2/js/script.js
@@ -0,0 +1,36 @@
+ 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    cssMode: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
